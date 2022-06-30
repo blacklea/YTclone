8f971a388ac35e71d745fcd0a11133804502e50e
Stefannie Scirgalea <102187435+blacklea@users.noreply.github.com>
2022-06-30 08:27:51 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-onload = function(){
-    document.querySelector('#toggler_btn').addEventListener('click', _=>{
-        document.querySelector('.content__container__aside').classList.toggle('open');
-    })
-}
